ad0762bd48827e71933b545fa4d21a9e794720e5
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-10-20 14:35:53 -0500

    Add: New file 0-rb_tree_node.c

--- 0x00-red_black_tree/0-rb_tree_node.c
+++ 0x00-red_black_tree/0-rb_tree_node.c
@@ -23,3 +23,66 @@ rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color)
 	node->right = NULL;
 	return (node);
 }
+
+/**
+ * inorder_successor - finds the inorder successor of given node
+ * @z: the node whose inorder successor to find
+ * Return: the inorder successor
+ */
+rb_tree_t *inorder_successor(rb_tree_t *z)
+{
+	rb_tree_t *y = z->right;
+
+	while (y && y->left)
+		y = y->left;
+	return (y);
+}
+
+/**
+ * rb_tree_remove_node - removes node from rb tree
+ * @tree: address of pointer to root of tree
+ * @z: node to remove
+ */
+void rb_tree_remove_node(rb_tree_t **tree, rb_tree_t *z)
+{
+	rb_tree_t *x, *x_parent = NULL, *y = z;
+	int y_color = y->color;
+
+	if (!z->left)
+	{
+		x = z->right;
+		x_parent = z->parent;
+		rb_tree_replace(tree, z, z->right);
+	}
+	else if (!z->right)
+	{
+		x = z->left;
+		x_parent = z->parent;
+		rb_tree_replace(tree, z, z->left);
+	}
+	else
+	{
+		y = inorder_successor(z);
+		y_color = y->color;
+		x = y->right;
+		if (y->parent == z)
+		{
+			x ? x->parent = y : 0;
+			x_parent = y;
+		}
+		else
+		{
+			rb_tree_replace(tree, y, y->right);
+			x_parent = y->parent;
+			y->right = z->right;
+			y->right->parent = y;
+		}
+		rb_tree_replace(tree, z, y);
+		y->left = z->left;
+		y->left->parent = y;
+		y->color = z->color;
+	}
+	if (y_color >= BLACK)
+		rb_tree_remove_fixup(tree, x, x_parent);
+	free(z);
+}
