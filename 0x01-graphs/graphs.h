1571c5022f3625713b8874119f82aa116f061416
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-11-30 16:55:10 -0500

    Feat(Update): New prototype added

--- 0x01-graphs/graphs.h
+++ 0x01-graphs/graphs.h
@@ -72,5 +72,7 @@ void graph_display(const graph_t *graph);
 
 graph_t *graph_create(void);
 vertex_t *graph_add_vertex(graph_t *graph, const char *str);
+int graph_add_edge(graph_t *graph, const char *src,
+		   const char *dest, edge_type_t type);
 
 #endif /* _GRAPHS_H_ */
