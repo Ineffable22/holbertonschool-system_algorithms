a7275f4dd7b26fa621ccd831e077434b6e3c6376
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2023-01-12 20:51:04 -0500

    Add: New file pathfinding.h

---
+++ 0x03-pathfinding/pathfinding.h
@@ -0,0 +1,26 @@
+#ifndef _PATHFINDING_H_
+#define _PATHFINDING_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "graphs.h"
+#include "queues.h"
+
+/**
+ * struct point_s - Structure storing coordinates
+ *
+ * @x: X coordinate
+ * @y: Y coordinate
+ */
+typedef struct point_s
+{
+	int x;
+	int y;
+} point_t;
+
+queue_t *backtracking_array(char **map, int rows, int cols,
+			    point_t const *start, point_t const *target);
+
+#endif /* _PATHFINDING_H_ */
