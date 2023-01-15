2eab7475cc86ac40ef9979ea7a25174be9f3ecec
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2023-01-14 20:55:08 -0500

    Feat(Update): New prototype added

--- 0x03-pathfinding/pathfinding.h
+++ 0x03-pathfinding/pathfinding.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 
 #include "graphs.h"
 #include "queues.h"
@@ -23,6 +24,8 @@ typedef struct point_s
 queue_t *backtracking_array(char **map, int rows, int cols,
 			    point_t const *start, point_t const *target);
 queue_t *backtracking_graph(graph_t *graph, vertex_t const *start,
+			    vertex_t const *target);
+queue_t *dijkstra_graph(graph_t *graph, vertex_t const *start,
 			    vertex_t const *target);
 
 #endif /* _PATHFINDING_H_ */
